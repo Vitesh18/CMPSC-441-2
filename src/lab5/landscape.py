8964f316624f2c976f41a6935750fbba92fd280a
Vitesh18 <105654569+Vitesh18@users.noreply.github.com>
2024-02-08 09:59:24 -0500

    Update landscape.py

--- src/lab5/landscape.py
+++ src/lab5/landscape.py
@@ -1,17 +1,24 @@
 import matplotlib.pyplot as plt
 from perlin_noise import PerlinNoise
 import numpy as np
+import random
 
 def get_elevation(size):
     xpix, ypix = size
     elevation = np.array([])
     '''Play around with perlin noise to get a better looking landscape (This is required for the lab)'''
+    noise = PerlinNoise(octaves=random.randint(1,20),seed=random.randint(3,9))
+    # xpix,ypix = size[0], size[1]
+    elevation = np.array([[noise([i/xpix, j/ypix])
+                        for j in range(ypix)]
+                        for i in range(xpix)]
+                        )
 
     return elevation
 
 def elevation_to_rgba(elevation):
     xpix, ypix = np.array(elevation).shape
-    colormap = plt.cm.get_cmap('gist_earth')
+    colormap = plt.cm.get_cmap('gist_stern')
     elevation = (elevation - elevation.min())/(elevation.max()-elevation.min())
     ''' You can play around with colormap to get a landscape of your preference if you want '''
     landscape = np.array([colormap(elevation[i, j])[0:3] for i in range(xpix) for j in range(ypix)]).reshape(xpix, ypix, 3)*255
@@ -25,5 +32,5 @@ get_landscape = lambda size: elevation_to_rgba(get_elevation(size))
 if __name__ == '__main__':
     size = 640, 480
     pic = elevation_to_rgba(get_elevation(size))
-    plt.imshow(pic, cmap='gist_earth')
+    plt.imshow(pic, cmap='gist_stern')
     plt.show()
