56a675cea3418fa490f65addeb49f2b6cbf4fc36
Vitesh18 <105654569+Vitesh18@users.noreply.github.com>
2024-03-14 13:14:47 -0400

    Update lab10.py

--- src/lab10/lab10.py
+++ src/lab10/lab10.py
@@ -9,8 +9,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
-data = pd.read_csv("src/lab8/heart.csv")
+data = pd.read_csv("GAME450_CMPSC441_Labs\src\lab10\heart.csv")
 
 # Transform the categorical variables into dummy variables.
 print(data.head())
@@ -26,12 +27,18 @@ x_train, x_test, y_train, y_test = train_test_split(
 
 """ Train a sklearn model here. """
 
-sklearn_model = None
+sklearn_model = KNeighborsClassifier(n_neighbors=5)
+sklearn_model.fit(x_train, y_train)
 
 # Accuracy
 print("Accuracy of model: {}\n".format(sklearn_model.score(x_test, y_test)))
 
 
 """ Improve the model by normalizing the input data. """
+scaler = StandardScaler()
+x_train_scaled = scaler.fit_transform(x_train)
+x_test_scaled = scaler.transform(x_test)
 
-print("Accuracy of improved model: {}\n".format(sklearn_model.score(x_test, y_test)))
+sklearn_model.fit(x_train_scaled, y_train)
+
+print("Accuracy of improved model: {}\n".format(sklearn_model.score(x_test_scaled, y_test)))
