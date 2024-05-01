ad43d23063dc9df02f2bb923bf77e36a5a07f532
Vitesh18 <105654569+Vitesh18@users.noreply.github.com>
2024-04-30 20:35:20 -0400

    Update pygame_ai_player.py

--- src/lab11/pygame_ai_player.py
+++ src/lab11/pygame_ai_player.py
@@ -1,23 +1,32 @@
 import pygame
 import random
-from pygame_human_player import PyGameHumanCombatPlayer  
-from turn_combat import CombatPlayer  
+import sys
+from pathlib import Path
+sys.path.append(str((Path(__file__) / ".." / "..").resolve().absolute()))
+from lab11.turn_combat import CombatPlayer
+from lab14.lab14 import global_journal  
+
 class PyGameAIPlayer:
-    def __init__(self, number_of_cities) -> None:
-        self.number_of_cities = number_of_cities
+    def __init__(self) -> None:
+        pass
 
     def selectAction(self, state):
-        if state.travelling:
-            return 48  # ASCII code for 0, meaning "keep going"
-        else:
-            # Selecting a random city that is not the current one
-            return ord(str((state.current_city + random.randint(1, self.number_of_cities - 1)) % self.number_of_cities))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if ord("0") <= event.key <= ord("9"):
+                    return event.key
+        
+        return ord(str(state.current_city))  # Not a safe operation for >10 cities
 
-class PyGameAICombatPlayer(PyGameHumanCombatPlayer):  
+
+class PyGameAICombatPlayer(CombatPlayer):  
     def __init__(self, name):
         super().__init__(name)
 
     def weapon_selecting_strategy(self):
         # Randomly select a weapon
         self.weapon = random.randint(0, 2)  # Choose among 0 (Sword), 1 (Arrow), or 2 (Fire)
+        global_journal.debug(f"AI decided to use {self.weapon}")
         return self.weapon
